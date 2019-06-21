2e5fb40cf4f023fce485eb2478bded1abc8883e9
MikeMaven <mikemaven@gmail.com>
2019-06-21 09:12:06 -0400

    integrated

---
+++ src/modules/donuts.js
@@ -0,0 +1,79 @@
+// Action type constants go here
+const NEW_DONUT_ORDER = 'NEW_DONUT_ORDER'
+
+const HANDLE_FIELD_CHANGE = 'HANDLE_FIELD_CHANGE'
+
+const CLEAR_FORM = 'CLEAR_FORM'
+
+// Action creators go here
+
+const handleFieldChange = (event) => {
+  const newValue = event.target.value
+  const field = event.target.name
+  return {
+    type: HANDLE_FIELD_CHANGE,
+    field,
+    newValue
+  }
+}
+
+const addNewDonutOrder = (donutOrder) => {
+  return {
+    type: NEW_DONUT_ORDER,
+    donutOrder
+  }
+}
+
+const clearForm = () => {
+  return {
+    type: CLEAR_FORM
+  }
+}
+
+
+// Reducer and its initialState go here
+
+const initialState = {
+  name: '',
+  flavor: '',
+  donutOrderList: [
+    {
+      id: 1,
+      name: 'Brianna',
+      flavor: 'Everything Bagel Doughnut'
+    },
+    {
+      id: 2,
+      name: "Alex",
+      flavor: 'Blackberry Hibiscus'
+    },
+    {
+      id: 3,
+      name: 'Dan',
+      flavor: 'The biggest coffee roll ever'
+    }
+  ]
+}
+
+const donuts = (state = initialState, action) => {
+  switch(action.type) {
+    case NEW_DONUT_ORDER:
+      const newDonuts = state.donutOrderList.concat(action.donutOrder)
+      return {...state, donutOrderList: newDonuts}
+    case CLEAR_FORM:
+      return {...state, name: '', flavor: ''}
+    case HANDLE_FIELD_CHANGE:
+      return {...state, [action.field]: action.newValue}
+    default:
+      return state
+  }
+};
+
+// Export statement goes here
+
+export {
+  donuts,
+  addNewDonutOrder,
+  handleFieldChange,
+  clearForm
+};
